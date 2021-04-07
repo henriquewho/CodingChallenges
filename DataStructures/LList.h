03abb1784fb16802fad7e09022fbb3bdcbc303ca
henriquewho <henriquegfonseca89@gmail.com>
2021-04-07 19:38:58 -0300

    practicing OO LList

---
+++ DataStructures/LList.h
@@ -0,0 +1,83 @@
+#ifndef LLIST_H
+#define LLIST_H
+
+// define the class Linked List, with a nested class Node
+
+template <typename Object> 
+class List {
+private: 
+    struct Node {
+        Object data; 
+        Node *prev; 
+        Node *next; 
+        Node (const Object &x = Object {}): data {x} {}
+    };
+
+private: 
+    int theSize=0;
+    Node *head; 
+    Node *tail; 
+
+public: 
+    List () : theSize {0} {
+        std::cout << "Main constructor called\n";
+        head = new Node;
+        tail = new Node; 
+        head->next = tail; 
+        tail->prev = head; 
+    }
+
+    void push_front(const Object &x){
+        theSize++; 
+        auto a = new Node(x); 
+
+        a->next = head->next; 
+        a->prev = head; 
+        head->next->prev = a; 
+        head->next = a; 
+    }
+
+    void push_back(const Object &x ){
+        theSize++; 
+        auto a = new Node(x); 
+
+        a->next = tail; 
+        a->prev = tail->prev; 
+        tail->prev->next = a; 
+        tail->prev = a;
+    }
+
+    Object pop_front(){
+        theSize--; 
+        head->next->next->prev = head; 
+        head->next = head->next->next; 
+    }
+
+    Object pop_back(){
+        theSize--; 
+        tail->prev->prev->next = tail; 
+        tail->prev = tail->prev->prev;
+    }
+
+    Object back(){
+        return tail->prev->data; 
+    }
+
+    Object front(){
+        return head->next->data; 
+    }
+
+    int size() const {return theSize;}
+
+    void print(){
+        auto temp = new Node;
+        temp = head->next; 
+
+        for (int i{}; i<theSize; i++){
+            std::cout << temp->data << std::endl;
+            temp = temp->next; 
+        }        
+    }
+};
+
+#endif
---
+++ DataStructures/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream> 
+#include <vector> 
+#include "LList.h"
+using namespace std; 
+
+void testLList(){
+    List<int> a;
+
+    cout << "Pushing Front, enter a number: "; 
+    int num; cin>>num; 
+    a.push_front(num);
+
+    cout << "Return front: " << a.front() << endl;
+
+    cout << "Pushing Back, enter a number: "; 
+    cin >> num; 
+    a.push_back(num); 
+
+    cout << "Return back: " << a.back() << endl; 
+
+    cout << "Size: " << a.size() << endl; 
+}
+
+void printLList(){
+    List<int> a; 
+
+    cout << "Enter 5 numbers: "; 
+    for (int i{}; i<5; i++){
+        int num;
+        cin >> num; 
+        a.push_back(num); 
+    }
+
+    a.pop_front(); a.pop_back(); 
+
+    cout << "Printing: "; a.print(); 
+
+}
+
+int main(){
+    //testLList();
+    printLList(); 
+}
